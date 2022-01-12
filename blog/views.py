3b48d2f1d796306d7296a4e24de970fe0f38c847
AlejandroR <alejandro.ramirez0596@gmail.com>
2022-01-12 20:58:05 +0100

    Creados los archivos forms.py y crear_post.html y modificado el views.py

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import FormularioPost #importamos el formulario Post
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("¡Hola, bienvenid@ al Blog!")
     return render(request, "blog.html")
+
+def crear_post(request):
+    form = FormularioPost()#aquí tendríamos el formulario
+    return render(request, "crear_post.html", {"form": form})#y se lo pasamos a la vista crear_post.html
 
